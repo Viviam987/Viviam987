f061c5b036154cb42efeaf5e6761b0eda630f000
Viviam987 <144297620+Viviam987@users.noreply.github.com>
2023-10-19 09:17:35 +0700

    Add files via upload

---
+++ nilai.js
@@ -0,0 +1,14 @@
+var nilai = 77;
+
+if nilai >77{
+    .document.write(Nilai B)
+}
+else if (nilai > 80 && nilai < 89){
+    grade ="B"
+} else if (nilai > 74 && nilai < 81){
+    grade = "C"
+} else {
+    grade = "D"
+}
+
+  document.write("nilai") + nilai + " masuk grade" + grade)
