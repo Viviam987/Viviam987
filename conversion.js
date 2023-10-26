b576427d58545189acdf30a3553119c46cb1e5f0
Viviam987 <144297620+Viviam987@users.noreply.github.com>
2023-10-26 09:06:22 +0700

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,28 @@
+var angkastring = '42';
+// merubah menjadi integer 
+var angkaInteger  = parseInt(angkaString);
+// merubah menjadi float (desimal) var angkaFloat = parseFloat(angkaString);
+var angkaFloat = perseFloat(angkastring);
+
+document.write("<h3>Conversion</h3>");
+document.write("string: " + angkaString + "<br>");
+document.write("Integer: " + angkaInteger + "<br>");
+document.write("Float: " + angkafloat + "<br>");
+
+//coercion (implicit type conversion)
+var angka= 10;
+var string "29";
+var hasil angka + string;
+
+document.write(<h3>coercion</h3>);
+document.write("angka: " angka + "<br>");
+document.write("string: " + string + "<br>)");
+document.writer("Hasil: " + hasil + "<br>);
+
+var boolen = true;
+var number = 42;
+var result = boolen + number + "<br>");
+
+document.write("Boolen: " + boolean + "<br>");
+document.write("Number: " + number + "<br>");
+document.write("Result: " ) + result + "<br>");
